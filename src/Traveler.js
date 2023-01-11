6307b099adb87185a9974d5cc034344a3f0a5175
Jesus Borjas <jesusbor006@gmail.com>
2023-01-11 15:11:13 -0600

    Add traveler class

---
+++ src/Traveler.js
@@ -0,0 +1,11 @@
+// This class gets the travelers
+class Traveler {
+  constructor(travelerData) {
+    this.id = travelerData.id;
+    this.name = travelerData.name;
+    this.travelerType = travelerData.travelerType
+  }
+}
+
+
+export default Traveler;
