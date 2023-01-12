85caee5cb314d108c1b52c6a0a49acc032031da1
Jesus Borjas <jesusbor006@gmail.com>
2023-01-12 12:40:23 -0600

    Add function to dynamically call fetched data when invoked in script file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,4 @@
 // Fetch Requests
-
 function fetchData(dataset) {
   return fetch(`http://localhost:3001/api/v1/${dataset}`).then(response => response.json()).catch(error => console.log(`${dataset}`, error))
 }
--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,108 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import TravelerRepository from './TraverlerRepository';
+import Traveler from './Traveler';
+import Trip from './Trip';
+import returnDataPromises from './apiCalls';
+import Destination from './Destinations';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png'
+
+// Query Selectors
+const travelerContainer = document.querySelector('.traveler-container');
+// const tripContainer = document.querySelector('.trips-container');
+// const destinationContainer = document.querySelector('.destinations-container');
+
+// Global Variables
+let travelerData;
+let tripData;
+let destinationData;
+let travelerRepository;
+let currentTraveler;
+let currentTrip;
+let currentDestination;
 
 
-// console.log('This is the JavaScript entry file - your code begins here.');
+// Functions
+function fetchApiCalls() {
+  returnDataPromises().then((data) => {
+    // travelerData gets all travelers
+    travelerData = (data[0].travelers.map((traveler) => new Traveler(traveler)))
+
+    // tripData fetches all trips     
+    tripData = data[1]
+
+    // destinationData fetches all destination data
+    destinationData = data[2].destinations.map((destination) => new Destination(destination))
+
+    // holds all Traveler Objects
+    travelerRepository = new TravelerRepository(travelerData)
+    console.log(travelerRepository)
+    
+    randomizeCurrentTraveler()
+    loadHandler()
+  })
+}
+
+function loadHandler() {
+  displayCurrentTravelerInfo();
+  getCurrentTrip()
+  // displayCurrentTripsInfo()
+  getCurrentDestination()
+  // displayCurrentDestinationInfo()
+}
+
+function getCurrentTrip() {
+  return currentTrip = tripData.trips.filter(trip => trip.userID === currentTraveler.id
+  )
+}
+
+function getCurrentDestination() {
+  return currentDestination = destinationData.filter(location => location.id === currentTraveler.id)
+}
+
+function randomIndex() {
+  return Math.floor(Math.random() * travelerData.length)
+}
+
+function randomizeCurrentTraveler() {
+  return currentTraveler = travelerRepository.travelers[randomIndex()]
+}
+
+// Event Listeners
+window.addEventListener("load", () => {
+  fetchApiCalls();
+});
+
+
+function displayCurrentTravelerInfo() {
+  travelerContainer.innerHTML = `
+    <p>Traveler ID: <span>${currentTraveler.id}</span></p>
+    <p>Traveler Name: <span>${currentTraveler.name}</span></p>
+    <p>Traveler Type: <span>${currentTraveler.travelerType}</span></p>
+`
+}
+
+// function displayCurrentTripsInfo() {
+//   tripContainer.innerHTML = `
+//     <p>Trip ID: <span>${allTripIDs}<span></p>
+//     <p>Traveler ID: <span>${currentTrip[0].userID}<span></p>
+//     <p>Destination ID: <span>${allDestinationIDs}</span></p>
+//     <p>Travelers: <span>${allTripTravelers}</span></p>
+//     <p>Date: <span>${allTripDates}</span></p>
+//     <p>Duration: <span>${allTripDuration}</span></p>
+//     <p>Status: <span>${allTripStatus}</span></p>
+//     <p>Suggested Activities: <span>"None At The Moment"</span></p>
+//   `
+// }
+
+// function displayCurrentDestinationInfo() {
+//   let allDestinationIDs2 = currentTrip.map(current => current.destinationID)
+//   let allDestinationData = currentDestination.map(current => current)
+//   destinationContainer.innerHTML = `
+//   <p>Destination ID: <span>${allDestinationIDs2}<span></p>
+//     <p>Destination: <span>${allDestinationData[0].destination}<span></p>
+//     <p>Estimated Lodging Cost Per Day: <span>${allDestinationData[0].estimatedLodgingCostPerDay}</span></p>
+//     <p>Estimated Flight Cost Per Person: <span>${allDestinationData[0].estimatedFlightCostPerPerson}</span></p>
+//     <p>Image: <span>${allDestinationData[0].image}</span></p>
+//     <p>Alt: <span>${allDestinationData[0].alt}</span></p>
+//   `
+// }
